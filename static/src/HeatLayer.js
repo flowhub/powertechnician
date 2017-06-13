fd2fa1e3ad5ffcd735ebe3d678df87e7c8a13596
Dierk Eichel <dietermeer@Dieters-MacBook-Pro.local>
2017-06-13 09:43:24 +0200

    new
    New

--- static/src/HeatLayer.js
+++ static/src/HeatLayer.js
@@ -2,13 +2,13 @@
 
 L.HeatLayer = (L.Layer ? L.Layer : L.Class).extend({
 
-    // options: {
-    //     minOpacity: 0.05,
-    //     maxZoom: 18,
-    //     radius: 25,
-    //     blur: 15,
-    //     max: 1.0
-    // },
+     options: {
+         minOpacity: 0.7,
+         maxZoom: 8,
+         radius: 10,
+         blur: 30,
+         max: 1.0
+    },
 
     initialize: function (latlngs, options) {
         this._latlngs = latlngs;
